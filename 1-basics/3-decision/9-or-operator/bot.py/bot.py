201e9cbcacc45fae16fd4d7345b5e30eef9bb2eb
4willl09 <4willl09@solent.ac.uk>
2019-10-02 11:08:25 +0000

    else if adventure

---
+++ 1-basics/3-decision/9-or-operator/bot.py/bot.py
@@ -0,0 +1,10 @@
+print("what adventure should I have")
+adventure = input()
+
+if ((adventure=="Short") or (adventure=="scary")):
+    print ("now entertering the dark woods")
+elif ((adventure=="safe") or (adventure=="long")):
+  print ("Taking the safe route!") 
+
+else:
+  print ("Not sure which route to take")
