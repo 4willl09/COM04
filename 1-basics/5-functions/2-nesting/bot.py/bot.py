98c6854f4d0409bd5796226a94a0db653066df97
4willl09 <4willl09@solent.ac.uk>
2019-10-16 08:55:22 +0000

    "Added code to show the use of a defined function with a nested decision

---
+++ 1-basics/5-functions/2-nesting/bot.py/bot.py
@@ -0,0 +1,10 @@
+def identify():
+    print ("What do you see?")
+    ans = input()
+    print (ans)
+    if (ans== "a large boulder"):
+     print ("time to run")
+    else:
+      print ("we will be Fine")
+
+identify()
